4215ed51ca01d297956154300bb8d7b1a08f6bf5
34177 <148678983+34177@users.noreply.github.com>
2025-04-17 21:10:37 +0800

    Update energy_community.py

--- energy_community.py
+++ energy_community.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
